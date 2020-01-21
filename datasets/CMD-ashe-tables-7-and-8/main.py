7500366a35b01b0103cb4c3413d5656b0f915547
James Bryant <jamesbryant792@gmail.com>
2020-01-21 16:20:44 +0000

    finished main.py

--- datasets/CMD-ashe-tables-7-and-8/main.py
+++ datasets/CMD-ashe-tables-7-and-8/main.py
@@ -16,10 +16,17 @@ df[:10]
 # ashe-statistics	statistics	ashe-sex	sex	ashe-working-pattern	workingpattern	
 # ashe-hours-and-earnings	hoursandearnings	ashe-workplace-or-residence	workplaceorresidence
 
+def MeasureTypeFunction(value):
+    if value.startswith('paid-hours-worked'):
+        return "Paid hours worked"
+    else:
+        return "Pay GBP"
+
 tidy_data = pd.DataFrame()
 
 tidy_data["Value"] = df["V4_2"]
-tidy_data[""]
+tidy_data["Marker"] = df["Data Marking"]
+tidy_data["CV"] = df["CV"]
 tidy_data["Year"] = df["time"]
 tidy_data["Geography"] = df["admin-geography"]
 tidy_data["ashe statistics"] = df["ashe-statistics"]
@@ -27,6 +34,7 @@ tidy_data["ashe sex"] = df["ashe-sex"]
 tidy_data["ashe working pattern"] = df["ashe-working-pattern"]
 tidy_data["ashe hours and earnings"] = df["ashe-hours-and-earnings"]
 tidy_data["ashe workplace or residence"] = df["ashe-workplace-or-residence"]
+tidy_data["Measure Type"] = tidy_data["ashe hours and earnings"].apply(MeasureTypeFunction)
 
 tidy_data[:10]
 
